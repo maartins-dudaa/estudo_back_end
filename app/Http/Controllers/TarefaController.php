65040d921ad18a0a24f4e591a52c4489c47faae4
maartins-dudaa <mmfmecma203@gmail.com>
2024-10-16 15:50:15 -0300

    first commit

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -14,17 +14,18 @@ class TarefaController extends Controller
             'descricao' => $request->descricao
 
         ]);
-         return response()->json([
+        return response()->json([
             'status' => true,
             'message' => 'Cliente cadastrado',
             'data' => $tarefa
-         ]);
+        ]);
     }
 
 
-    public function findById($id){
+    public function findById($id)
+    {
         $tarefa = Tarefa::find($id);
-        if($tarefa == null){
+        if ($tarefa == null) {
             return response()->json([
                 'status' => false,
                 'message' => 'Tarefa não encontrada'
@@ -36,9 +37,10 @@ class TarefaController extends Controller
         ]);
     }
 
-    public function findByIdRequest(Request $request){
+    public function findByIdRequest(Request $request)
+    {
         $tarefa = Tarefa::find($request->id);
-        if($tarefa == null){
+        if ($tarefa == null) {
             return response()->json([
                 'status' => false,
                 'message' => 'Tarefa não encontrada'
@@ -49,7 +51,96 @@ class TarefaController extends Controller
             'status' => true,
             'data' => $tarefa
         ]);
-
     }
-    
+
+
+
+    public function getAll()
+    {
+        $tarefa = Tarefa::all();
+        return response()->json([
+            'status' => true,
+            'data' => $tarefa
+        ]);
+    }
+
+
+    public function show($id)
+    {
+        $tarefa = Tarefa::find($id);
+        if ($tarefa == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Tarefa não encontrada',
+                'data' => $tarefa
+            ]);
+        }
+        return response()->json([
+            'status' => true,
+            'message' => 'Tarefa encontrada',
+            'data' => $tarefa
+        ]);
+    }
+
+
+    public function delete(Request $request)
+    {
+        $tarefa = Tarefa::find($request->id);
+        if ($tarefa == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'tarefa não encontrada'
+            ]);
+        }
+        $tarefa->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'tarefa deletada',
+            'data' => $tarefa
+        ]);
+    }
+
+
+
+    public function update(Request $request)
+    {
+        $tarefa = Tarefa::find($request->id);
+        if ($tarefa == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'tarefa não encontrada'
+            ]);
+        }
+
+        if (isset($request->titulo)) {
+            $tarefa->titulo = $request->titulo;
+        }
+        if (isset($request->descricao)) {
+            $tarefa->descricao = $request->descricao;
+        }
+        if (isset($request->status)) {
+            $tarefa->status = $request->status;
+        }
+        $tarefa->update();
+        return response()->json([
+            'status' => true,
+            'message' => 'tarefa atualizada',
+            'data' => $tarefa
+        ]);
+    }
+
+    public function pesquisarPorDiaEMes(Request $request)
+    {
+        $tarefa = Tarefa::whereMonth('created_at', '=', $request->mes)->whereDay('created_at', '=', $request->dia)->get();
+        if ($tarefa->isEmpty()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'sem resultados para a pesquisa'
+            ]);
+        }
+        return response()->json([
+            'status' => true,
+            'data' => $tarefa
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,14 @@ Route::post('tarefa/cadastro', [TarefaController::class,  'cadastrar']);
 Route::get('tarefa/{id}', [TarefaController::class, 'findById']);
 
 Route::get('tarefa/find/id', [TarefaController::class, 'findByIdRequest']);
+
+Route::get('tarefa/get/all', [TarefaController::class, 'getAll']);
+
+Route::get('tarefa/show/{id}', [TarefaController::class, 'show']);
+
+Route::delete('tarefa/delete', [TarefaController::class, 'delete']);
+
+
+Route::put('tarefa/update', [TarefaController::class, 'update']);
+
+Route::get('tarefa/pesquisa/dia/mes', [TarefaController::class, 'pesquisarPorDiaEMes']);
